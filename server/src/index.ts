7b036d98cbd250625de57b6af3f5ffba79e3c2cc
Бахти <softbrains@MacBook-Air-Bahti.local>
2024-09-22 23:05:41 +0500

    serves

--- server/src/index.ts
+++ server/src/index.ts
@@ -37,7 +37,7 @@ app.use("/teams", teamRoutes)
 
 // server
 
-const port = process.env.PORT || 3000
-app.listen(port, () => {
+const port = Number(process.env.PORT) || 3000
+app.listen(port, "0.0.0.0", () => {
     console.log(`Server running on PORT ${port}`)
 })
