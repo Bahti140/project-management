420d754f85c1a97f6fc16c5c31b46a58fb91f0f3
Бахти <softbrains@MacBook-Air-Bahti.local>
2024-09-22 22:19:59 +0500

    server some fixes

---
+++ client/src/app/priority/low/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import React from 'react'
+import ReusablePriorityPage from '../reusablePriorityPage'
+import { Priority } from '@/src/state/api'
+
+
+const Urgent = () => {
+  return (
+    <ReusablePriorityPage priority={Priority.Low} />
+  )
+}
+
+export default Urgent
